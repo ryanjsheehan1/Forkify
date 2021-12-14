2e4ba13e544e19771ca76082efcf9f0c73771380
Ryan Sheehan <ryanjsheehan1@gmail.com>
2021-12-14 13:25:18 -0600

    Initial commit

---
+++ src/js/controller.js
@@ -0,0 +1,34 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+
+import 'core-js/stable'; // Polyfilling
+import 'regenerator-runtime/runtime'; // Polyfilling async/await
+
+const recipeContainer = document.querySelector('.recipe');
+
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // 1) Loading Recipe
+    await model.loadRecipe(id);
+
+    // 2) Rendering Recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    recipeView.renderError();
+  }
+};
+
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+init();
